2719c3e04b6cf0bc319176f6b2ad8bed1ddce2e5
Daniel Ellis <1346302+dellis23@users.noreply.github.com>
2022-10-01 22:20:58 -0400

    Add some audios.

--- js/audio.js
+++ js/audio.js
@@ -11,18 +11,45 @@ var SOUNDS = {
 			new Audio("sounds/bleep_lo_2.mp3"),
 		],
 	],
+	"success": [
+		[
+			new Audio("sounds/success_hi_01.mp3"),
+			new Audio("sounds/success_hi_02.mp3"),
+			new Audio("sounds/success_hi_03.mp3"),
+		],
+		[
+			new Audio("sounds/success_mid_01.mp3"),
+			new Audio("sounds/success_mid_02.mp3"),
+			new Audio("sounds/success_mid_03.mp3"),
+		],
+		[
+			new Audio("sounds/success_low_01.mp3"),
+			new Audio("sounds/success_low_02.mp3"),
+			new Audio("sounds/success_low_03.mp3"),
+		],
+	],
+	"spaceship_falling": [
+		[
+			new Audio("sounds/spaceship_falling.mp3"),
+		],
+	]
 }
 
 // TODO: Preload?
 
-export function play(name) {
+export function play(name, vary_pitch, volume) {
 	var layers = SOUNDS[name];
+	volume = volume === undefined ? 1.0 : volume;
 	for (const layer of layers) {
 		var sound = layer[utils.get_random_item(layer)];
 	    sound.mozPreservesPitch = false;
 	    sound.preservesPitch = false;
-	    sound.playbackRate = utils.get_random_in_range(0.8, 1.2);
-	    sound.volume = utils.get_random_in_range(0.8, 1.0);
+	    if (vary_pitch) {
+	    	sound.playbackRate = utils.get_random_in_range(0.99, 1.01);
+	    } else {
+	    	sound.playbackRate = 1.0;
+	    }
+	    sound.volume = utils.get_random_in_range(0.9, 1.0) * volume;
 	    sound.currentTime = 0;
 	    sound.play();
 	}
